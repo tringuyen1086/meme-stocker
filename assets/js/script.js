9b3542c7460dee419a972be1e9b530e9cc9f0529
Joshua Lintz <jplintz@gmail.com>
2021-12-10 07:19:08 -0800

    initialize modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,37 @@
-var url = "https://tradestie.com/api/v1/apps/reddit";
+var openmodal = document.querySelectorAll('.modal-open')
+for (var i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener('click', function(event){
+   event.preventDefault()
+   toggleModal()
+  })
+}
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+const overlay = document.querySelector('.modal-overlay')
+overlay.addEventListener('click', toggleModal)
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+var closemodal = document.querySelectorAll('.modal-close')
+for (var i = 0; i < closemodal.length; i++) {
+  closemodal[i].addEventListener('click', toggleModal)
+}
 
-xhr.send();
+document.onkeydown = function(evt) {
+  evt = evt || window.event
+  var isEscape = false
+  if ("key" in evt) {
+   isEscape = (evt.key === "Escape" || evt.key === "Esc")
+  } else {
+   isEscape = (evt.keyCode === 27)
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+   toggleModal()
+  }
+};
+
+
+function toggleModal () {
+  const body = document.querySelector('body')
+  const modal = document.querySelector('.modal')
+  modal.classList.toggle('opacity-0')
+  modal.classList.toggle('pointer-events-none')
+  body.classList.toggle('modal-active')
+}
