7187ae53e17e9f4d126ff6cf2e23bdc5774b4c8c
Joshua Lintz <jplintz@gmail.com>
2021-12-09 19:18:49 -0800

    add wallstreetbets api var

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var url = "https://tradestie.com/api/v1/apps/reddit";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
