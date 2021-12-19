806708b8f700a3b6ba6f8f8f5af9f0127159058f
Tri Nguyen <tri.nguyen1086@gmail.com>
2021-12-18 18:59:57 -0800

    modified HTLM and JavaScript to match up Develop and Main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
+// open modal for any ticker symbol given
+function openModal(tickerSymbol) {
+  fmpApi(tickerSymbol);
+  toggleModal();
+}
 var openmodal = document.querySelectorAll('.modal-open')
 for (var i = 0; i < openmodal.length; i++) {
   openmodal[i].addEventListener('click', function(event){
-   event.preventDefault()
-   toggleModal()
+    event.preventDefault();
+    // call open modal with symbol from teh search input textbox
+    openModal(document.getElementById('tickersymbol').value);
   })
 }
-
 const overlay = document.querySelector('.modal-overlay')
 overlay.addEventListener('click', toggleModal)
-
 var closemodal = document.querySelectorAll('.modal-close')
 for (var i = 0; i < closemodal.length; i++) {
   closemodal[i].addEventListener('click', toggleModal)
 }
-
 document.onkeydown = function(evt) {
   evt = evt || window.event
   var isEscape = false
@@ -26,60 +29,18 @@ document.onkeydown = function(evt) {
    toggleModal()
   }
 };
-
-
 function toggleModal () {
   const body = document.querySelector('body')
   const modal = document.querySelector('.modal')
   modal.classList.toggle('opacity-0')
   modal.classList.toggle('pointer-events-none')
   body.classList.toggle('modal-active')
+  clearModal()
 }
-
-var openmodal = document.querySelectorAll('.modal-open')
-for (var i = 0; i < openmodal.length; i++) {
-  openmodal[i].addEventListener('click', function(event){
-   event.preventDefault()
-   toggleModal()
-  })
-}
-
-const overlay = document.querySelector('.modal-overlay')
-overlay.addEventListener('click', toggleModal)
-
-var closemodal = document.querySelectorAll('.modal-close')
-for (var i = 0; i < closemodal.length; i++) {
-  closemodal[i].addEventListener('click', toggleModal)
-}
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event
-  var isEscape = false
-  if ("key" in evt) {
-   isEscape = (evt.key === "Escape" || evt.key === "Esc")
-  } else {
-   isEscape = (evt.keyCode === 27)
-  }
-  if (isEscape && document.body.classList.contains('modal-active')) {
-   toggleModal()
-  }
-};
-
-
-function toggleModal () {
-  const body = document.querySelector('body')
-  const modal = document.querySelector('.modal')
-  modal.classList.toggle('opacity-0')
-  modal.classList.toggle('pointer-events-none')
-  body.classList.toggle('modal-active')
-}
-
 // Wallstreetbets API
-const url = 'https://tradestie.com/api/v1/apps/reddit';
-
+const url = 'https://cors-proxy-blah.herokuapp.com/get-json/?url=http://tradestie.com/api/v1/apps/reddit';
 // Make request to URL
 fetch(url)
-
   .then(function (response) {
     return response.json();
 })
@@ -87,12 +48,10 @@ fetch(url)
   .then(function (data) {
     appenddata(data);
   })
-
   // Catch any errors that may occur
   .catch(function (error) {
     console.log(error);
   });
-
 // Sort data by Sentiment Score
 function appenddata(data) {
   let sortdata = data.sort((firstEl, secondEl) => {
@@ -104,12 +63,21 @@ function appenddata(data) {
         return 0;
     }
 });
-
   // Dynamically fill divs with JSON data
   var mainContainer = document.getElementById("mstocks");
   for (var i = 0; i < 10; i++) {
-    var div = document.createElement("div");    
-    div.innerHTML = sortdata[i].ticker
+    // this is required because of javasript callback insanity
+    let sym = sortdata[i].ticker;
+    // turn this into an anchor for clicky experience
+    var div = document.createElement("a");    
+    div.href = "";
+    div.onclick = (ev) => {
+      // dont actually do anything but run openModal using the ticker symobol clicked on
+      ev.preventDefault();
+      openModal(sym);
+    }
+    div.innerHTML = sym;
+    
     var div2 = document.createElement("div");   
     div2.innerHTML = sortdata[i].sentiment_score;
     mainContainer.appendChild(div);
@@ -117,24 +85,41 @@ function appenddata(data) {
   }
 }
 // Add Yahoo API link
-function yahoolink() {
-  document.getElementById("mstocks").innerHTML = (url)}
-
-// FMP API call
-var nething = "AAPL";
-var ticker = document.getElementById('tickersymbol').value;
-var requestUrl = 'https://financialmodelingprep.com/api/v3/profile/MSFT?apikey=203f41c2d13b0a556884a3a115113e59';
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (stonkData) {
-    console.log('Your stonk');
-    console.log(stonkData);
-    console.log(stonkData[0].companyName);
-    console.log(stonkData[0].ceo);
-    console.log(stonkData[0].symbol)
-  });
-  console.log(ticker)
-
-
+//function yahoolink() {
+  //document.getElementById("mstocks").innerHTML = (url)}
+// FMP API call 
+function fmpApi(tickerSymbol){
+  // added this so that we can pass in the ticker symbol instead of only using what's in the search input textbox
+  var requestUrl = 'https://financialmodelingprep.com/api/v3/profile/'+ tickerSymbol +'?apikey=203f41c2d13b0a556884a3a115113e59';
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (stonkData) {
+      console.log('Your stonk');
+      console.log(stonkData);
+      document.getElementById("modal-header").innerHTML = stonkData[0].companyName
+      const objectArray = Object.entries(stonkData[0]);
+      var modalBody = document.getElementById("modal-body")
+      for (x=0;x<10;x++){
+        console.log(objectArray[x])
+        var label = document.createElement('div');
+        var stonkValue = document.createElement('div');
+        label.innerHTML = objectArray[x][0];
+        stonkValue.innerHTML = objectArray[x][1]
+        modalBody.appendChild(label)
+        modalBody.appendChild(stonkValue)
+      }
+      storeTickers();
+    });
+}
+// Clear Modal content
+function clearModal(){
+  var modalBody = document.getElementById("modal-body")
+  modalBody.innerHTML = "";
+}
+// store search to local storage 
+function storeTickers(){
+ var symbol = document.getElementById('tickersymbol').value;
+ var storeTicker = window.localStorage.setItem('ticker', symbol);
+}
